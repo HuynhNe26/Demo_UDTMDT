aefa801950313d096ede114110e26b8dbcfe1ea2
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-10-05 23:26:21 +0700

    update

---
+++ routes/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/routerGet', (req, res) => {
+    res.send('<h1>Ví dụ lấy dữ liệu</h1>');
+});
+
+router.post('/routerPost', (req, res) => {
+    const data = req.body; 
+    res.json({
+        message: 'Đã nhận dữ liệu thành công!',
+        receivedData: data
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const routerGet = require('./routes/router');
+
+app.use('/', routerGet);
+
+// Khởi động server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
